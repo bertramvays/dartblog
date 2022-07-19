73c902e69926c39948bd09fefae45986d1b98276
Dmitrij <grenko_d@protonmail.com>
2022-07-19 20:39:35 +0300

    Реализация поиска на странице поста не удалась. Реализовали облако тегов и популярные статьи на странице поста.

--- siteblog/blog/templatetags/sidebar.py
+++ siteblog/blog/templatetags/sidebar.py
@@ -1,10 +1,14 @@
-from django import templates
+from django import template
 from blog.models import Post, Tag
 
-register = Template.Library()
+register = template.Library()
 
 @register.inclusion_tag('blog/popular_posts_tpl.html')
 def get_popular(cnt=3):
-    post = Post.objects.order_by('-views')[:cnt]
-    return {"post": posts}
+    posts = Post.objects.order_by('-views')[:cnt]
+    return {"posts": posts}
 
+@register.inclusion_tag('blog/tags_tpl.html')
+def get_tags():
+    tags = Tag.objects.all()
+    return {"tags": tags}
